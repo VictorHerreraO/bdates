94b2bd6341c2650e39d9a9e1a28dd9fc40f095a4
Victor Herrera <vherrera@overcome.mx>
2022-04-27 10:48:58 -0500

    Mostrar datos reales en el listado

--- app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/framework/presentation/EventListViewModel.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/framework/presentation/EventListViewModel.kt
@@ -10,6 +10,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,18 +24,51 @@ class EventListViewModel @Inject constructor(
     val events: LiveData<List<EventViewState>>
         get() = _events
 
+    private val today: LocalDate = LocalDate.now()
+    private val longFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("EE, dd/MM")
+
     init {
         viewModelScope.launch {
             getEventListUseCase(Unit).collect { events ->
-                _events.value = events.sortedBy { event ->
-                    event.monthOfYear
+
+                _events.value = events.map { event ->
+                    // Add event occurrences
+                    val currentYearOccurrence = LocalDate.of(
+                        today.year,
+                        event.monthOfYear,
+                        event.dayOfMonth
+                    )
+                    event.copy(
+                        currentYearOccurrence = currentYearOccurrence,
+                        nextOccurrence = if (currentYearOccurrence.isAfter(today)) currentYearOccurrence
+                        else currentYearOccurrence.plusYears(1L)
+                    )
+                }.sortedBy { event ->
+                    // Sort by upcoming
+                    event.nextOccurrence
                 }.map { event ->
+                    // Map to View State
+                    val nextOccurrence = event.nextOccurrence!!
                     EventViewState(
                         id = event.id,
-                        remainingTimeValue = "0",
+                        remainingTimeValue = ChronoUnit.DAYS
+                            .between(today, nextOccurrence)
+                            .toString(),
                         remainingTimeUnit = "Días",
                         name = event.name,
-                        description = "${event.dayOfMonth} / ${event.monthOfYear}"
+                        description = nextOccurrence.let { date ->
+                            val formatted = date.format(longFormatter)
+                            return@let event.year?.let { birthYear ->
+                                val birthDate = LocalDate.of(
+                                    birthYear,
+                                    date.month,
+                                    date.dayOfMonth
+                                )
+                                "$formatted • Cumple " + ChronoUnit.YEARS
+                                    .between(birthDate, nextOccurrence)
+                                    .toString()
+                            } ?: formatted
+                        }
                     )
                 }
             }
