0edb82df0562fc94884539df075b54427ebf5c0d
Victor Herrera <victor.herrera@wizeline.com>
2023-02-23 13:07:27 -0600

    #5: added KVP store

---
+++ app/src/main/java/com/soyvictorherrera/bdates/core/persistence/KeyValueStore.kt
@@ -0,0 +1,49 @@
+package com.soyvictorherrera.bdates.core.persistence
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import javax.inject.Inject
+import kotlin.reflect.KClass
+
+const val APP_SHARED_PREFS_FILE_NAME = "app_preferences"
+
+class Key<T : Any> internal constructor(val name: String, val type: KClass<T>)
+
+fun stringKey(name: String): Key<String> = Key(name, String::class)
+fun booleanKey(name: String): Key<Boolean> = Key(name, Boolean::class)
+
+interface KeyValueStoreContract {
+    operator fun <T : Any> set(key: Key<T>, value: T)
+    operator fun <T : Any> get(key: Key<T>): T?
+}
+
+class KeyValueStore @Inject constructor(
+    private val sharedPreferences: SharedPreferences
+) : KeyValueStoreContract {
+
+    override fun <T : Any> set(key: Key<T>, value: T) {
+        sharedPreferences.edit {
+            when (key.type) {
+                String::class -> putString(key.name, value as String)
+                Boolean::class -> putBoolean(key.name, value as Boolean)
+                else -> throw IllegalArgumentException("Unknown key type: ${key.type}")
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override operator fun <T : Any> get(key: Key<T>): T? {
+        val contained = sharedPreferences.contains(key.name)
+        return if (contained) {
+            with(sharedPreferences) {
+                when (key.type) {
+                    String::class -> getString(key.name, null)
+                    Boolean::class -> getBoolean(key.name, false)
+                    else -> throw IllegalArgumentException("Unknown key type: ${key.type}")
+                }
+            } as T
+        } else {
+            null
+        }
+    }
+}
--- app/src/main/java/com/soyvictorherrera/bdates/core/persistence/PersistenceModule.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/core/persistence/PersistenceModule.kt
@@ -1,7 +1,10 @@
 package com.soyvictorherrera.bdates.core.persistence
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.room.Room
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,14 +13,29 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object PersistenceModule {
-    @Provides
-    @Singleton
-    fun provideAppDatabase(application: Application): AppDatabase {
-        return Room.databaseBuilder(
-            application,
-            AppDatabase::class.java,
-            APP_DATABASE_NAME
-        ).build()
+abstract class PersistenceModule {
+
+    @Binds
+    abstract fun bindKeyValueStoreContract(keyValueStore: KeyValueStore): KeyValueStoreContract
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideAppDatabase(application: Application): AppDatabase {
+            return Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                APP_DATABASE_NAME
+            ).build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideSharedPreferences(application: Application): SharedPreferences {
+            return application.getSharedPreferences(
+                APP_SHARED_PREFS_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        }
     }
 }
