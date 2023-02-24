54c5c82507ee488ad7190fab6b5b5ffafb374dfd
Victor Herrera <victor.herrera@wizeline.com>
2023-02-24 17:33:21 -0600

    #5: added compose

---
+++ app/src/main/java/com/soyvictorherrera/bdates/core/compose/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.soyvictorherrera.bdates.core.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Alabaster = Color(0xFFF8F8F8)
+val Bittersweet = Color(0xFFFF6961)
+val Black = Color(0xFF000000)
+val Bossanova = Color(0xFF3C2A59)
+val Cod_Gray = Color(0xFF191919)
+val Cyan_light = Color(0xFF4fb3bf)
+val Dolphin = Color(0xFF695C7E)
+val Gallery = Color(0xFFEEEEEE)
+val Rajah = Color(0xFFF6AF65)
+val Tradewind = Color(0xFF6EBEB1)
+val White = Color(0xFFFFFFFF)
+val Paradiso = Color(0xFF388A7C)
