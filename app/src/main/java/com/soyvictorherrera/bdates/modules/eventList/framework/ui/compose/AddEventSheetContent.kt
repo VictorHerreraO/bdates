f0fc46ee5a23f34c7dcf2177dfbd06b0b7b94f51
Victor Herrera <victor.herrera@wizeline.com>
2023-02-28 17:16:47 -0600

    #5: added capitalization for event name

--- app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/framework/ui/compose/AddEventSheetContent.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/framework/ui/compose/AddEventSheetContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -28,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.soyvictorherrera.bdates.R
@@ -142,6 +144,7 @@ private fun EventNameSection(
             cursorColor = MaterialTheme.colors.secondary,
             focusedIndicatorColor = MaterialTheme.colors.secondary
         ),
+        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
     )
 }
 
