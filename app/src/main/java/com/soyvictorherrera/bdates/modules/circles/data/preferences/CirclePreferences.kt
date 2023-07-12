8c83cfe32cab97270da019015b53f7ae0a632879
Victor Herrera <vic_h_o@hotmail.com>
2023-07-11 18:24:23 -0600

    #18: wip: get event list based on timestamps

--- app/src/main/java/com/soyvictorherrera/bdates/modules/circles/data/preferences/CirclePreferences.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/modules/circles/data/preferences/CirclePreferences.kt
@@ -1,13 +1,17 @@
 package com.soyvictorherrera.bdates.modules.circles.data.preferences
 
+import com.soyvictorherrera.bdates.core.persistence.Key
 import com.soyvictorherrera.bdates.core.persistence.KeyValueStoreContract
 import com.soyvictorherrera.bdates.core.persistence.booleanKey
 import com.soyvictorherrera.bdates.core.persistence.stringKey
+import com.soyvictorherrera.bdates.modules.eventList.data.datasource.assets.forEach
 import javax.inject.Inject
+import org.json.JSONObject
 
 interface CirclePreferencesContract {
     var isLocalCircleCreated: Boolean
     var localCircleId: String?
+    val updateTimestamps: KeyValuePreference<Long>
 }
 
 class CirclePreferences @Inject constructor(
@@ -16,6 +20,7 @@ class CirclePreferences @Inject constructor(
     private companion object {
         val IS_LOCAL_CIRCLE_CREATED_KEY = booleanKey("is_local_circle_created")
         val LOCAL_CIRCLE_ID_KEY = stringKey("local_circle_id")
+        val UPDATE_TIMESTAMPS = stringKey("update_timestamps")
     }
 
     override var isLocalCircleCreated: Boolean
@@ -29,4 +34,46 @@ class CirclePreferences @Inject constructor(
         set(value) {
             store[LOCAL_CIRCLE_ID_KEY] = value
         }
+
+    override val updateTimestamps: KeyValuePreference<Long> = KeyValueLongPreference(
+        store = store,
+        storeKey = UPDATE_TIMESTAMPS
+    )
+}
+
+interface KeyValuePreference<T> {
+    operator fun set(key: String, value: T?)
+    operator fun get(key: String): T?
+}
+
+internal class KeyValueLongPreference(
+    private val store: KeyValueStoreContract,
+    private val storeKey: Key<String>,
+) : KeyValuePreference<Long> {
+    private val valueJSON: JSONObject
+
+    init {
+        val valueString = store[storeKey].orEmpty()
+        valueJSON = try {
+            JSONObject(valueString)
+        } catch (ex: Exception) {
+            JSONObject()
+        }
+        valueJSON.names()?.forEach {
+            this[it as String] = valueJSON.getLong(it)
+        }
+    }
+
+    override fun set(key: String, value: Long?) {
+        valueJSON.put(key, value)
+        valueJSON.sync()
+    }
+
+    override fun get(key: String): Long? {
+        return valueJSON.optLong(key)
+    }
+
+    private fun JSONObject.sync() {
+        store[storeKey] = toString()
+    }
 }
--- app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/domain/usecase/UpdateEventsUseCase.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/modules/eventList/domain/usecase/UpdateEventsUseCase.kt
@@ -1,8 +1,12 @@
 package com.soyvictorherrera.bdates.modules.eventList.domain.usecase
 
 import com.soyvictorherrera.bdates.core.arch.UseCase
+import com.soyvictorherrera.bdates.core.date.DateProviderContract
+import com.soyvictorherrera.bdates.core.date.toEpochMilli
 import com.soyvictorherrera.bdates.core.network.Resource
+import com.soyvictorherrera.bdates.modules.circles.data.preferences.CirclePreferencesContract
 import com.soyvictorherrera.bdates.modules.circles.data.repository.CircleRepositoryContract
+import com.soyvictorherrera.bdates.modules.circles.domain.model.Circle
 import com.soyvictorherrera.bdates.modules.eventList.domain.model.Event
 import javax.inject.Inject
 import timber.log.Timber
@@ -11,24 +15,61 @@ interface UpdateEventsUseCaseContract : UseCase<Unit, Resource<List<Event>>>
 
 class UpdateEventsUseCase @Inject constructor(
     private val circlesRepo: CircleRepositoryContract,
+    private val circlePrefs: CirclePreferencesContract,
+    private val dateProvider: DateProviderContract,
 ) : UpdateEventsUseCaseContract {
     override suspend fun execute(params: Unit): Resource<List<Event>> {
-        when (val resource = circlesRepo.getCircles()) {
-            is Resource.Error -> {
-                Timber.e(resource.cause, "error while fetching the circles")
-                Timber.d("Local circles are:")
-                resource.data?.forEach { Timber.d("circle: $it") }
-            }
-
-            is Resource.Loading -> {
-                // no-op
-            }
-
-            is Resource.Success -> {
-                Timber.d("Updated circles:")
-                resource.data.forEach { Timber.d("circle: $it") }
-            }
+        fetchCircles().forEach {
+            fetchCircleEvents(it)
         }
         return Resource.Success(emptyList())
     }
+
+    private suspend fun fetchCircles(): List<Circle> {
+        return when (val resource = circlesRepo.getCircles()) {
+            is Resource.Success -> {
+                resource.data
+            }
+
+            else -> {
+                if (resource is Resource.Error) {
+                    Timber.e(resource.cause)
+                }
+                emptyList()
+            }
+        }
+    }
+
+    private suspend fun fetchCircleEvents(circle: Circle) {
+        if (circle.isLocalOnly) {
+            Timber.d("circle ${circle.name} is local only")
+            return
+        }
+        if (circle.isUpToDate) {
+            Timber.d("circle ${circle.name} is up-to date")
+            return
+        }
+
+        val now = dateProvider.currentLocalDateTime.toEpochMilli()
+        val lastCheck = circle.lastUpdateCheck ?: 0
+
+        Timber.d("Working with ${circle.name}")
+        Timber.d("last check was: $lastCheck")
+        Timber.d("Once refreshed will update with millis = $now")
+
+        circle.lastUpdateCheck = now
+    }
+
+    private var Circle.lastUpdateCheck: Long?
+        get() = circlePrefs.updateTimestamps[id!!]
+        set(value) {
+            circlePrefs.updateTimestamps[id!!] = value
+        }
+
+    private val Circle.isUpToDate: Boolean
+        get() {
+            val lastCheck = lastUpdateCheck ?: 0
+            val lastUpdate = updateDate ?: 0
+            return lastCheck >= lastUpdate
+        }
 }
