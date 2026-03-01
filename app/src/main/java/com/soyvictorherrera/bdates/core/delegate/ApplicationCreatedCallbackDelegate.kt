d1087febc2993194fa158936f1d2bcc04e3ad924
Victor Herrera <vic_h_o@hotmail.com>
2026-03-01 10:49:53 -0700

    #1: Update project configurations and app config stability
    Co-authored-by: Antigravity <antigravity@google.com>

--- app/src/main/java/com/soyvictorherrera/bdates/core/delegate/ApplicationCreatedCallbackDelegate.kt
+++ app/src/main/java/com/soyvictorherrera/bdates/core/delegate/ApplicationCreatedCallbackDelegate.kt
@@ -3,6 +3,7 @@ package com.soyvictorherrera.bdates.core.delegate
 import com.soyvictorherrera.bdates.core.coroutines.MainCoroutineScope
 import com.soyvictorherrera.bdates.modules.appinfo.domain.AppInfoProvider
 import com.soyvictorherrera.bdates.modules.notifications.NotificationManagerContract
+import com.soyvictorherrera.bdates.modules.appConfig.AppConfigContract
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
